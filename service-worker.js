d4c7a8041456ba3689268b1f4848f23afc2f8106
Kilian-star <kilian.mro@outlook.de>
2025-12-02 15:45:01 +0100

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,16 @@
+const CACHE_NAME = 'kuchen-cache-v1';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/css/styles.css',
+  '/js/script.js',
+  '/rezepte.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
+});
+self.addEventListener('activate', event => { event.waitUntil(self.clients.claim()); });
+self.addEventListener('fetch', event => {
+  event.respondWith(caches.match(event.request).then(resp => resp || fetch(event.request)));
+});
